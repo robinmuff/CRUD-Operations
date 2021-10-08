63918944c3f352efc6a8414d289ae85f28a83780
Robin Muff <robin.muff@bluewin.ch>
2021-10-08 18:01:44 +0200

    Update to new open structure

---
+++ CRUD-Operations/CRUD-Default.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CRUD_Operations
+{
+    public abstract class CRUDDefault<O>
+    {
+        ObservableCollection<O> list;
+
+        public CRUDDefault()
+        {
+            list = new ObservableCollection<O>();
+
+            list.CollectionChanged += List_CollectionChanged;
+        }
+
+        public ObservableCollection<O> List
+        {
+            get
+            {
+                if (list == null || list.Count == 0) { readList(); }
+                return list;
+            }
+            set
+            {
+                list = value;
+                safeList();
+            }
+        }
+        private void List_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            safeList();
+        }
+
+        /* -- File Operations -- */
+        public abstract void safeList();
+        public abstract void readList();
+    }
+}
