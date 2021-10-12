096d2f7691ae59a107a86aabb75c543718fde131
Robin Muff <robin.muff@bluewin.ch>
2021-10-12 09:23:11 +0200

    Remove EntityCore

---
+++ CRUD-Operations-EntityCore/CRUD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Operations_EntityCore
+{
+    public class CRUD<O> : CRUD_Operations.CRUDDefault<O>
+    {
+        public CRUD()
+        {
+            
+            readList();
+        }
+
+        public override void readList()
+        {
+            
+        }
+        public override void safeList()
+        {
+            
+        }
+    }
+}
